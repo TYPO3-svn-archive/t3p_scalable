9bc8680f7c739dc974d883bfd983ecf090284e46
ohader <ohader@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-06-15 14:22:23 +0000

    Fixed bug: Defined checks for TYPO3 4.2.0-4.5.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3p_scalable/trunk@21418 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,6 +9,9 @@ require_once t3lib_extMgm::extPath($_EXTKEY) . 'class.tx_t3pscalable.php';
 	// Define XCLASS accordant to current TYPO3 version:
 switch (TYPO3_branch) {
 	case '4.2':
+	case '4.3':
+	case '4.4':
+	case '4.5':
 		$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['t3lib/class.t3lib_db.php'] = t3lib_extMgm::extPath($_EXTKEY) . 'typo3versions/4.2.FF/class.ux_t3lib_db.php';
 		break;
 
