038f19500298e1e753a703fff8cdaa094ea0bf49
ohader <ohader@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-06-15 14:28:43 +0000

    Cleanup: Remove structures that depent on specific TYPO3 versions
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3p_scalable/trunk@21420 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,23 +5,6 @@ if (!defined('TYPO3_MODE')) {
 
 	// Include the base class for t3p_scalable:
 require_once t3lib_extMgm::extPath($_EXTKEY) . 'class.tx_t3pscalable.php';
-
 	// Define XCLASS accordant to current TYPO3 version:
-switch (TYPO3_branch) {
-	case '4.2':
-	case '4.3':
-	case '4.4':
-	case '4.5':
-		$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['t3lib/class.t3lib_db.php'] = t3lib_extMgm::extPath($_EXTKEY) . 'typo3versions/4.2.FF/class.ux_t3lib_db.php';
-		break;
-
-		// unsupported version. do nothing:
-	default:
-		t3lib_div::sysLog(
-			$_EXTKEY . ' is not compatible to the TYPO3 version you are running!',
-			$_EXTKEY,
-			3
-		);
-}
-
+$TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['t3lib/class.t3lib_db.php'] = t3lib_extMgm::extPath($_EXTKEY) . 'class.ux_t3lib_db.php';
 ?>
