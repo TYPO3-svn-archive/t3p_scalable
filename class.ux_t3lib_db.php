34d82635ce9a894d660acf5d86ac76b228571c7c
axeljung01 <axeljung01@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-11-19 10:38:53 +0000

    Fix critical bug: t3lib_DB::sql_error always returned last error from DB-Master (even if DB-SLAVE was used to execute SQL-SELECT-statement)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3p_scalable/trunk@81619 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.ux_t3lib_db.php
+++ class.ux_t3lib_db.php
@@ -31,16 +31,28 @@
  */
 class ux_t3lib_DB extends t3lib_DB {
 	/**
+	 * @var integer
+	 */
+	const LINK_MASTER = 1;
+	/**
+	 * @var integer
+	 */
+	const LINK_SLAVE = 2;
+
+	/**
 	 * The default link resource (write/master databases)
 	 * @var	resource
 	 */
 	public $link = FALSE;
-
 	/**
 	 * The alternative link resoure (read/slave databases) 
 	 * @var	resource
 	 */
 	public $linkRead = FALSE;
+	/**
+	 * @var integer
+	 */
+	private $lastUsedLink = self::LINK_MASTER;
 
 	/**
 	 * The controlling t3p_scalable object
@@ -55,7 +67,124 @@ class ux_t3lib_DB extends t3lib_DB {
 		$this->t3pscalable = t3lib_div::makeInstance('tx_t3pscalable');
 	}
 
+	/**
+	 * Returns information about the character sets supported by the current DBM
+	 * This function is important not only for the Install Tool but probably for
+	 * DBALs as well since they might need to look up table specific information
+	 * in order to construct correct queries. In such cases this information should
+	 * probably be cached for quick delivery.
+	 *
+	 * This is used by the Install Tool to convert tables tables with non-UTF8 charsets
+	 * Use in Install Tool only!
+	 *
+	 * @return	array		Array with Charset as key and an array of "Charset", "Description", "Default collation", "Maxlen" as values
+	 */
+	function admin_get_charsets() {
+		$this->lastUsedLink = self::LINK_MASTER;
+		return parent::admin_get_charsets();
+	}
+	/**
+	 * Returns information about each field in the $table (quering the DBMS)
+	 * In a DBAL this should look up the right handler for the table and return compatible information
+	 * This function is important not only for the Install Tool but probably for
+	 * DBALs as well since they might need to look up table specific information
+	 * in order to construct correct queries. In such cases this information should
+	 * probably be cached for quick delivery.
+	 *
+	 * @param	string		Table name
+	 * @return	array		Field information in an associative array with fieldname => field row
+	 */
+	function admin_get_fields($tableName) {
+		$this->lastUsedLink = self::LINK_MASTER;
+		return parent::admin_get_fields($tableName);
+	}
+	/**
+	 * Returns information about each index key in the $table (quering the DBMS)
+	 * In a DBAL this should look up the right handler for the table and return compatible information
+	 *
+	 * @param	string		Table name
+	 * @return	array		Key information in a numeric array
+	 */
+	function admin_get_keys($tableName) {
+		$this->lastUsedLink = self::LINK_MASTER;
+		return parent::admin_get_keys($tableName);
+	}
+	/**
+	 * Returns the list of tables from the default database, TYPO3_db (quering the DBMS)
+	 * In a DBAL this method should 1) look up all tables from the DBMS  of
+	 * the _DEFAULT handler and then 2) add all tables *configured* to be managed by other handlers
+	 * Usage count/core: 2
+	 *
+	 * @return	array		Array with tablenames as key and arrays with status information as value
+	 */
+	function admin_get_tables() {
+		$this->lastUsedLink = self::LINK_MASTER;
+		return parent::admin_get_tables();
+	}
+	/**
+	 * mysql() wrapper function, used by the Install Tool and EM for all queries regarding management of the database!
+	 * Usage count/core: 10
+	 *
+	 * @param	string		Query to execute
+	 * @return	pointer		Result pointer
+	 */
+	function admin_query($query) {
+		$this->lastUsedLink = self::LINK_MASTER;
+		return parent::admin_query($query);
+	}
 
+	/**
+	 * Creates and executes a DELETE SQL-statement for $table where $where-clause
+	 * Usage count/core: 40
+	 *
+	 * @param	string		Database tablename
+	 * @param	string		WHERE clause, eg. "uid=1". NOTICE: You must escape values in this argument with $this->fullQuoteStr() yourself!
+	 * @return	pointer		MySQL result pointer / DBAL object
+	 */
+	function exec_DELETEquery($table, $where) {
+		$this->lastUsedLink = self::LINK_MASTER;
+		return parent::exec_DELETEquery($table, $where);
+	}
+	/**
+	 * Creates and executes an INSERT SQL-statement for $table with multiple rows.
+	 *
+	 * @param	string		Table name
+	 * @param	array		Field names
+	 * @param	array		Table rows. Each row should be an array with field values mapping to $fields
+	 * @param	string/array		See fullQuoteArray()
+	 * @return	pointer		MySQL result pointer / DBAL object
+	 */
+	public function exec_INSERTmultipleRows($table, array $fields, array $rows, $no_quote_fields = FALSE) {
+		$this->lastUsedLink = self::LINK_MASTER;
+		return parent::exec_INSERTmultipleRows($table, $fields, $rows, $no_quote_fields);
+	}
+	/**
+	 * Creates and executes an INSERT SQL-statement for $table from the array with field/value pairs $fields_values.
+	 * Using this function specifically allows us to handle BLOB and CLOB fields depending on DB
+	 * Usage count/core: 47
+	 *
+	 * @param	string		Table name
+	 * @param	array		Field values as key=>value pairs. Values will be escaped internally. Typically you would fill an array like "$insertFields" with 'fieldname'=>'value' and pass it to this function as argument.
+	 * @param	string/array		See fullQuoteArray()
+	 * @return	pointer		MySQL result pointer / DBAL object
+	 */
+	function exec_INSERTquery($table, $fields_values, $no_quote_fields = FALSE) {
+		$this->lastUsedLink = self::LINK_MASTER;
+		return parent::exec_INSERTquery($table, $fields_values, $no_quote_fields);
+	}
+	/**
+	 * Executes a prepared query.
+	 * This method may only be called by t3lib_db_PreparedStatement.
+	 *
+	 * @param string $query The query to execute
+	 * @param array $queryComponents The components of the query to execute
+	 * @return pointer MySQL result pointer / DBAL object
+	 * @access private
+	 */
+	public function exec_PREPAREDquery($query, array $queryComponents) {
+		$this->lastUsedLink = self::LINK_MASTER;
+		return parent::exec_PREPAREDquery($query, $queryComponents);
+	}
 	/**
 	 * Creates and executes a SELECT SQL-statement
 	 * Using this function specifically allow us to handle the LIMIT feature independently of DB.
@@ -70,6 +199,7 @@ class ux_t3lib_DB extends t3lib_DB {
 	 */
 	public function exec_SELECTquery($select_fields, $from_table, $where_clause, $groupBy='', $orderBy='', $limit='') {
 		if (!$this->t3pscalable->isAssuredWriteBackendSession() && !$this->t3pscalable->isAssuredWriteCliDispatch() && !$this->t3pscalable->isAssuredWriteTable($from_table)) {
+			$this->lastUsedLink = self::LINK_SLAVE;
 			$query = $this->SELECTquery($select_fields, $from_table, $where_clause, $groupBy, $orderBy, $limit);
 			$res = mysql_query($query, $this->linkRead);
 
@@ -80,28 +210,66 @@ class ux_t3lib_DB extends t3lib_DB {
 				$this->explain($query, $from_table, $this->sql_num_rows($res));
 			}
 		} else {
-			$res = $this->exec_SELECTquery_master($select_fields, $from_table, $where_clause, $groupBy, $orderBy, $limit);
+			$this->lastUsedLink = self::LINK_MASTER;
+			$res = parent::exec_SELECTquery($select_fields, $from_table, $where_clause, $groupBy, $orderBy, $limit);
 		}
 
 		return $res;
 	}
-
 	/**
-	 * Creates and executes a SELECT SQL-statement on the master/regular link
-	 * Using this function specifically allow us to handle the LIMIT feature independently of DB.
+	 * Truncates a table.
 	 *
-	 * @param	string		List of fields to select from the table. This is what comes right after "SELECT ...". Required value.
-	 * @param	string		Table(s) from which to select. This is what comes right after "FROM ...". Required value.
-	 * @param	string		Optional additional WHERE clauses put in the end of the query. NOTICE: You must escape values in this argument with $this->fullQuoteStr() yourself! DO NOT PUT IN GROUP BY, ORDER BY or LIMIT!
-	 * @param	string		Optional GROUP BY field(s), if none, supply blank string.
-	 * @param	string		Optional ORDER BY field(s), if none, supply blank string.
-	 * @param	string		Optional LIMIT value ([begin,]max), if none, supply blank string.
+	 * @param	string		Database tablename
+	 * @return	mixed		Result from handler
+	 */
+	public function exec_TRUNCATEquery($table) {
+		$this->lastUsedLink = self::LINK_MASTER;
+		return parent::exec_TRUNCATEquery($table);
+	}
+	/**
+	 * Creates and executes an UPDATE SQL-statement for $table where $where-clause (typ. 'uid=...') from the array with field/value pairs $fields_values.
+	 * Using this function specifically allow us to handle BLOB and CLOB fields depending on DB
+	 * Usage count/core: 50
+	 *
+	 * @param	string		Database tablename
+	 * @param	string		WHERE clause, eg. "uid=1". NOTICE: You must escape values in this argument with $this->fullQuoteStr() yourself!
+	 * @param	array		Field values as key=>value pairs. Values will be escaped internally. Typically you would fill an array like "$updateFields" with 'fieldname'=>'value' and pass it to this function as argument.
+	 * @param	string/array		See fullQuoteArray()
 	 * @return	pointer		MySQL result pointer / DBAL object
 	 */
-	public function exec_SELECTquery_master($select_fields, $from_table, $where_clause, $groupBy='', $orderBy='', $limit='') {
-		return parent::exec_SELECTquery($select_fields, $from_table, $where_clause, $groupBy, $orderBy, $limit);
+	function exec_UPDATEquery($table, $where, $fields_values, $no_quote_fields = FALSE) {
+		$this->lastUsedLink = self::LINK_MASTER;
+		return parent::exec_UPDATEquery($table, $where, $fields_values, $no_quote_fields);
 	}
 
+	/**
+	 * Executes query
+	 * mysql() wrapper function
+	 * Usage count/core: 0
+	 *
+	 * @param	string		Database name
+	 * @param	string		Query to execute
+	 * @return	pointer		Result pointer / DBAL object
+	 * @deprecated since TYPO3 3.6, will be removed in TYPO3 4.6
+	 * @see sql_query()
+	 */
+	function sql($db, $query) {
+		$this->lastUsedLink = self::LINK_MASTER;
+		return parent::sql($db, $query);
+	}
+	/**
+	 * Returns the error status on the last sql() execution
+	 * mysql_error() wrapper function
+	 * Usage count/core: 32
+	 *
+	 * @return	string		MySQL error string.
+	 */
+	function sql_error() {
+		if($this->lastUsedLink === self::LINK_SLAVE) {
+			return mysql_error($this->linkRead);
+		}
+		return mysql_error($this->link);
+	}
 	/**
 	 * Open a (persistent) connection to a MySQL server
 	 * mysql_pconnect() wrapper function
@@ -153,9 +321,22 @@ class ux_t3lib_DB extends t3lib_DB {
 			}
 		}
 
-		return $this->linkRead;
+		return $this->link;
 	}
-
+	/**
+	 * Executes query
+	 * mysql_query() wrapper function
+	 * Beware: Use of this method should be avoided as it is experimentally supported by DBAL. You should consider
+	 *         using exec_SELECTquery() and similar methods instead.
+	 * Usage count/core: 1
+	 *
+	 * @param	string		Query to execute
+	 * @return	pointer		Result pointer / DBAL object
+	 */
+	function sql_query($query) {
+		$this->lastUsedLink = self::LINK_MASTER;
+		return parent::sql_query($query);
+	}
 	/**
 	 * Select a MySQL database
 	 * mysql_select_db() wrapper funct
@@ -183,5 +364,4 @@ class ux_t3lib_DB extends t3lib_DB {
 if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/t3p_scalable/class.ux_t3lib_db.php']) {
 	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/t3p_scalable/class.ux_t3lib_db.php']);
 }
-
 ?>
